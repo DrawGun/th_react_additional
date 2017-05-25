ad41d0a29183fbc30639b663960fa2267193665d
DrawGun <drawgun@yandex.ru>
2017-05-26 00:44:04 +0300

    lesson_1: Реализовал серверный рендеринг

---
+++ components/ui/Image.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Image extends React.Component {
+
+  render() {
+    const {
+      src,
+      width,
+      height,
+      alt
+    } = this.props;
+
+    return (
+      <div>
+        <img
+          src={src}
+          width={width}
+          height={height}
+          alt={alt} />
+      </div>
+    )
+  }
+
+}
+
+export default Image;
---
+++ components/ui/TextEl.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TextBox from './TextBox';
+
+const TextEl = ({ text }) => <div>
+  <TextBox>
+    {text}
+  </TextBox>
+</div>
+
+export default TextEl;
